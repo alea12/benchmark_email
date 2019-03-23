ab7afb02f682e51d9b4746ea4c75aff041eadaa1
alea12 <mail@alea12.net>
2019-03-23 15:55:02 +0900

    $ bundler gem benchmark_email ✨

---
+++ lib/benchmark_email.rb
@@ -0,0 +1,6 @@
+require "benchmark_email/version"
+
+module BenchmarkEmail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/benchmark_email/version.rb
@@ -0,0 +1,3 @@
+module BenchmarkEmail
+  VERSION = "0.1.0"
+end
